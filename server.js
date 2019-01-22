6e6487ac00fe39295f62bea7feec6b8c60001b20
Glitch (imaginary-supermarket) <none>
2019-01-22 05:00:36 +0000

    Updated with Glitch

---
+++ server.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var Url = require('./urlShortenerController.js').UrlModel;
+var createUrl = require('./urlShortenerController.js').createUrl;
+var findByShortUrl = require('./urlShortenerController.js').findByShortUrl;
+var dns = require('dns');
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(cors());
+
+// Error handler
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }
+});
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGO_URI);
+
+// URL to check mongodb connection status
+app.get('/is-mongoose-ok', function(req, res) {
+  if (mongoose) {
+    res.json({isMongooseOk: !!mongoose.connection.readyState})
+  } else {
+    res.json({isMongooseOk: false})
+  }
+});
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
+
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+// API endpoints 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// User stories 1 & 2
+app.post("/api/shorturl/new", function(req, res) {
+  const url = req.body.url;
+  let lookUpUrl = url.replace(/^https?:\/\//i, "");
+  lookUpUrl = lookUpUrl.replace(/\/.+/, "");
+
+  dns.lookup(lookUpUrl, function (err, addresses, family) {
+    if (url.match(/^https?:\/\//i) && addresses) {
+      createUrl(url, function(err, data) {
+        res.json({ original_url: data.originalUrl, short_url: data.shortUrl });
+      });
+    } else {
+      res.json({ "error": "invalid URL" });
+    }
+  });
+
+});
+
+// User story 3.
+app.get("/api/shorturl/:url", function(req, res) {
+  findByShortUrl(req.params.url, function(err, data) {
+    if (data) {
+      res.status(301).redirect(data.originalUrl);
+    } else {
+      res.json({ "error": "invalid short URL or original URL not yet shortened" });
+    }
+  });
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
---
+++ urlShortenerController.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGO_URI);
+
+
+const schema = new Schema({
+    originalUrl: { type: String, required: true },
+    shortUrl: { type: Number, required: true },
+});
+
+const Url = mongoose.model('Url', schema);
+
+
+const maxIndexUrl = () => {
+  return Url
+          .findOne()
+          .sort('-shortUrl')
+          .exec();
+}
+
+const findOriginalUrl = (url) => {
+  return Url
+          .findOne({ originalUrl: url })
+          .exec();
+}
+
+const findShortUrl = (url) => {
+  return Url
+          .findOne({ shortUrl: url })
+          .exec();
+}
+
+async function createAndSaveUrl(url, done) {
+  try {
+    const maxIndUrl = await maxIndexUrl();
+
+    var maxInd;
+    if (maxIndUrl === null) {
+      maxInd = 0;
+    } else {
+      maxInd = maxIndUrl.shortUrl + 1;
+    }
+
+    const newUrl = new Url({ originalUrl: url, shortUrl: maxInd });
+    newUrl.save(function(err, data) {
+      if(err) {
+        return done(err) 
+      };
+      done(null, data);
+    });
+  } catch (err) {
+    console.log(err); 
+  }
+};
+
+async function createUrl(url, done) {
+  try {
+    const foundUrl = await findOriginalUrl(url);
+
+    if (foundUrl === null) {
+      createAndSaveUrl(url, done);
+    } else {
+      done(null, foundUrl);  
+    }
+  } catch (err) {
+    console.log(err); 
+  }
+}
+
+async function findByShortUrl(url, done) {
+  try {
+    const foundUrl = await findShortUrl(url);
+    done(null, foundUrl);
+  } catch (err) {
+    console.log(err); 
+  }
+}
+
+exports.UrlModel = Url;
+exports.findByShortUrl = findByShortUrl;
+exports.createUrl = createUrl;
